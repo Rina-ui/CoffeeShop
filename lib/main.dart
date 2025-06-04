b5895901b07daa36f3443bc5e795bd3561bbe924
Rina-ui <marinagracia895@gmail.com>
2025-06-04 22:29:28 +0200

    Screen Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:coffeeshop/home.dart';
+
 import 'package:flutter/material.dart';
 
 import 'views/HomePage.dart';
--- lib/views/StartUp_screen.dart
+++ lib/views/StartUp_screen.dart
@@ -6,7 +6,35 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(decoration: BoxDecoration(),),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/téléchargement.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+
+        child: Column(
+          children: [
+            SizedBox(height: 100,),
+            Image.asset('assets/images/coffeeImage.png',
+            width: 150,
+            fit: BoxFit.cover,
+            ),
+
+            SizedBox(height: 20,),
+
+            Text("Magic Coffee",
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w300,
+              color: Colors.white,
+            )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
