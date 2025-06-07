e1b444f76ad2bf7edd69cda385eedeb0a96063e5
Rina-ui <marinagracia895@gmail.com>
2025-06-07 21:19:36 +0200

    Mapcard and Sreen page

---
+++ lib/views/MapCard.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class Mapcard extends StatefulWidget {
+  const Mapcard({super.key});
+
+  @override
+  State<Mapcard> createState() => _MapcardState();
+}
+
+class _MapcardState extends State<Mapcard> {
+
+  void initState() {
+    super.initState();
+
+    //un delai pour naviger directement sur une autre page
+    Future.delayed(Duration(seconds: 10), () {
+      Navigator.push(
+        context,
+         MaterialPageRoute(builder: (context) => Mapcard())
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(6.1319, 1.2228),
+          minZoom: 5,
+          maxZoom: 20,
+        ),
+
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+            userAgentPackageName: 'com.example.map',
+          ),
+
+          MarkerLayer(
+              markers: [
+                Marker(
+                    point: LatLng(6.1319, 1.2228),
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.centerLeft,
+                    child: Icon(Icons.location_pin,
+                      color: Colors.red,
+                      size: 50,)
+                )
+              ]
+          )
+        ],
+      ),
+    );;
+  }
+}
+
--- lib/views/StartUp_screen.dart
+++ lib/views/StartUp_screen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Screen extends StatelessWidget {
+import 'MapCard.dart';
+
+class Screen extends StatefulWidget {
   const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  void initState() {
+    super.initState();
+
+    //un delai de 10 secondes et va ver la page Mapcard
+    Future.delayed(Duration(seconds: 10), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Mapcard()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,32 +28,32 @@ class Screen extends StatelessWidget {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/téléchargement.jpg'),
-          fit: BoxFit.cover,
+          image: DecorationImage(
+            image: AssetImage('assets/images/téléchargement.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
 
         child: Column(
           children: [
             SizedBox(height: 100,),
             Image.asset('assets/images/coffeeImage.png',
-            width: 150,
-            fit: BoxFit.cover,
+              width: 150,
+              fit: BoxFit.cover,
             ),
 
             SizedBox(height: 20,),
 
             Text("Magic Coffee",
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.w300,
-              color: Colors.white,
-            )
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                )
             )
           ],
         ),
       ),
-    );
+    );;
   }
 }
